62e187c764329e3aef1aca830a4e7d10b901abc6
Hidde Boekema <boekemah@gmail.com>
2019-10-16 13:01:57 +0100

    Updates to the segmentation_with_generator experiment

--- experiments/segmentation_with_generator/segment.py
+++ experiments/segmentation_with_generator/segment.py
@@ -1,25 +1,28 @@
 #!usr/bin/env/python3
 
 import os
+import json
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import segmentation_models as sm
 import keras
+from keras.callbacks import LambdaCallback
 import glob
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 from data_generator import DataGenerator
 
 keras.backend.set_image_data_format('channels_last')
 
-data_dir = '/data/cvfs/hjhb2/data/silhouettes'
+data_dir = './data/'
 
 # Load the test and training data
-x_train_dir = os.path.join(data_dir, 'train_img/')
-y_train_dir = os.path.join(data_dir, 'train_labels/')
-x_test_dir = os.path.join(data_dir, 'test_img/')
+x_train_dir = os.path.join(data_dir, 'train_img_subset/')
+y_train_dir = os.path.join(data_dir, 'train_labels_subset/')
+x_test_dir = os.path.join(data_dir, 'fotis-vid489-256/')
 
 n_channels = 3
 
@@ -72,6 +75,13 @@ X_test /= 255
 BACKBONE = 'resnet34'
 preprocess_input = sm.get_preprocessing(BACKBONE)
 
+# Test inputs
+X_test = [cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(x_test_dir + "*.png")]
+X_test = X_test[:20]
+X_test = np.array(X_test)
+X_test = X_test.reshape((X_test.shape[0], 256, 256, n_channels))
+x_test = preprocess_input(X_test)
+
 """
 # preprocess input
 x_train = preprocess_input(X_train)
@@ -95,6 +105,25 @@ params = {'dim': (256, 256),
 # Define generators
 training_generator = DataGenerator(x_train_dir, y_train_dir, **params)
 
+# Callback functions
+# Stream the epoch loss to a file in JSON format. The file content
+# is not well-formed JSON but rather has a JSON object per line.
+log_path = "./logs/loss_log_{}".format(datetime)
+epoch_log = open(log_path, mode='wt', buffering=1)
+json_logging_callback = LambdaCallback(
+    on_epoch_end=lambda epoch, logs: eval_epoch(epoch_log, epoch, logs),
+    on_train_end=lambda logs: epoch_log.close()
+)
+
+
+def eval_epoch(epoch_log, epoch, logs):
+    """ Write loss to a log and predict on sample images """
+    epoch_log.write(
+        json.dumps({'epoch': epoch, 'loss': logs['loss']}) + '\n')
+    cv2.imwrite()
+
+
+
 # Define model
 model = sm.Unet(BACKBONE, encoder_weights='imagenet', input_shape=(None, None, n_channels))
 model.compile('Adam', loss=sm.losses.binary_crossentropy, metrics=[sm.metrics.iou_score])
@@ -103,16 +132,9 @@ print("Fitting model...")
 # Fit model
 model.fit_generator(
     generator=training_generator,
-    epochs=10,
+    epochs=1,
     use_multiprocessing=True,
-    workers=6
 )
-
-X_test = [cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(x_test_dir + "*.png")]
-X_test = X_test[:20]
-X_test = np.array(X_test)
-X_test = X_test.reshape((X_test.shape[0], 256, 256, n_channels))
-x_test = preprocess_input(X_test)
 
 print("Model fit. Predicting...")
 preds = model.predict(x_test)
